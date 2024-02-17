9dd9a94589723c16eb817f6be7a0c1b46d1c3e93
Nikhil Shenoy <nikhilshenoy98@gmail.com>
2024-02-16 23:46:00 -0800

    updated readme

--- model.py
+++ model.py
@@ -3,10 +3,21 @@ import torch.nn.functional as F
 from torch import nn
 from utils import Trainer
 
+# Some global parameters
+# ----------------------
+block_size = 32
+dropout = 0.2
+n_layer = 6
+max_iters = 10000
+batch_size = 12
+eval_interval = 2000
+eval_iters = 200
+eval_interval = 2000
+# ----------------------
 
 class SingleHeadAttention(nn.Module):
     '''Single Head Attention Implementation'''
-    def __init__(self, n_embd: int, head_dim: int, block_size: int):
+    def __init__(self, n_embd: int, head_dim: int):
         super().__init__()
         self.key_layer = nn.Linear(n_embd, head_dim, bias=False)
         self.query_layer = nn.Linear(n_embd, head_dim, bias=False)
@@ -32,18 +43,23 @@ class SingleHeadAttention(nn.Module):
 
 class MultiHeadAttention(nn.Module):
     '''Multi-Head Attention Implementation'''
-    def __init__(self, n_embd: int, head_dim: int, block_size: int, n_heads: int = 4):
+    def __init__(self, n_embd: int, head_dim: int, n_heads: int = 4):
         super().__init__()
         self.n_heads = n_heads
+        self.head_dim = head_dim
         self.key_layer = nn.Linear(n_embd, head_dim, bias=False)
         self.query_layer = nn.Linear(n_embd, head_dim, bias=False)
         self.value_layer = nn.Linear(n_embd, head_dim, bias=False)
         self.register_buffer('tril', torch.tril(torch.ones(block_size, block_size)))
 
+        self.proj = nn.Linear(head_dim, head_dim)
+        self.dropout = nn.Dropout(dropout)
+
     def forward(self, x):
         # shape of x: (B, T, n_embd)
-        B, T, C = x.shape
+        B, T, _ = x.shape
         H = self.n_heads
+        C = self.head_dim
         # (B, H, T, head_dim // H)
         q = self.query_layer(x).view(B, T, H, -1).transpose(1, 2)
         k = self.key_layer(x).view(B, T, H, -1).transpose(1, 2)
@@ -54,10 +70,12 @@ class MultiHeadAttention(nn.Module):
         wei = torch.einsum('ijkl,ijlm->ijkm', q, k.transpose(-2, -1)) * C ** (-0.5) # (B, H, T, T)
         wei = wei.masked_fill(self.tril[:T, :T] == 0, float("-inf"))
         wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = self.dropout(wei)
 
         # apply attention to value
         # (B, H, T, T) @ (B, H, T, head_dim) -> (B, H, T, head_dim)
-        return torch.einsum('ijkl,ijlm->ijkm', wei, v).transpose(1, 2).reshape(B, T, C)
+        out = torch.einsum('ijkl,ijlm->ijkm', wei, v).transpose(1, 2).reshape(B, T, C)
+        return self.dropout(self.proj(out))
     
 
 class BiGramModel(nn.Module):
@@ -96,13 +114,13 @@ class BiGramModel(nn.Module):
         return output
 
 
-class BiGramWithSingleHeadAttention(nn.Module):
-    def __init__(self, vocab_size: int, n_embd: int, head_dim: int, block_size: int):
+class BiGramWithAttention(nn.Module):
+    def __init__(self, vocab_size: int, n_embd: int, head_dim: int):
         super().__init__()
-        self.block_size = block_size
         self.token_embedding = nn.Embedding(vocab_size, n_embd)
         self.position_embedding = nn.Embedding(block_size, n_embd)
-        self.single_head_attention = MultiHeadAttention(n_embd, head_dim, block_size)
+        self.attn = MultiHeadAttention(n_embd, head_dim, block_size)
+        self.ln = nn.LayerNorm(head_dim)
         self.lm_head = nn.Linear(head_dim, vocab_size)
 
     def forward(self, input: torch.Tensor):
@@ -111,7 +129,8 @@ class BiGramWithSingleHeadAttention(nn.Module):
         token_emb = self.token_embedding(input) # (B, T, n_embd)
         pos_emb = self.position_embedding(torch.arange(T, device=input.device)) # (T, n_embd)
         x = token_emb + pos_emb # (B, T, n_embd)
-        x = self.single_head_attention(x) # (B, T, head_dim)
+        x = self.attn(x) # (B, T, head_dim)
+        x = self.ln(x) # (B, T, head_dim)
         return self.lm_head(x) # (B, T, vocab_size)
 
     def compute_loss(self, logits: torch.Tensor, target: torch.Tensor):
@@ -129,24 +148,94 @@ class BiGramWithSingleHeadAttention(nn.Module):
         '''
         output = input # (B, T)
         for _ in range(max_len):
-            logits = self(output[:, -self.block_size:]) # (B, T, C)
+            logits = self(output[:, -block_size:]) # (B, T, C)
             probs = F.softmax(logits[:, -1, :], dim=-1) # (B, C)
             next_token = torch.multinomial(probs, 1) # (B, 1)
             output = torch.cat([output, next_token], dim=1) # (B, T + 1)
 
-        return output    
+        return output
+
+
+class FeedForward(nn.Module):
+    def __init__(self, head_dim: int):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(head_dim, 4 * head_dim),
+            nn.ReLU(),
+            nn.Linear(4 * head_dim, head_dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    def __init__(self, n_embd: int, head_dim: int, n_heads: int = 4):
+        super().__init__()
+        self.attn = MultiHeadAttention(n_embd, head_dim, n_heads)
+        self.ff = FeedForward(head_dim)
+        self.ln1 = nn.LayerNorm(head_dim)
+        self.ln2 = nn.LayerNorm(head_dim)
+
+    def forward(self, x):
+        # Note: Residual Connection
+        x = x + self.attn(self.ln1(x))
+        return x + self.ff(self.ln2(x))
+
+
+class BiGramWithBlocks(nn.Module):
+    def __init__(self, vocab_size: int, n_embd: int, head_dim: int):
+        super().__init__()
+        self.token_embedding = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding = nn.Embedding(block_size, n_embd)
+        self.attn = nn.Sequential(*[Block(n_embd, head_dim, n_heads=4)] * n_layer)
+        self.ln = nn.LayerNorm(head_dim)
+        self.lm_head = nn.Linear(head_dim, vocab_size)
+
+    def forward(self, input: torch.Tensor):
+        # input: (B, T)
+        B, T = input.shape
+        token_emb = self.token_embedding(input) # (B, T, n_embd)
+        pos_emb = self.position_embedding(torch.arange(T, device=input.device)) # (T, n_embd)
+        x = token_emb + pos_emb # (B, T, n_embd)
+        x = self.attn(x) # (B, T, head_dim)
+        x = self.ln(x) # (B, T, head_dim)
+        return self.lm_head(x) # (B, T, vocab_size)
+
+    def compute_loss(self, logits: torch.Tensor, target: torch.Tensor):
+        C = logits.shape[2]
+        return F.cross_entropy(
+            logits.view(-1, C), # pytorch needs channels as the last dimension
+            target.view(-1),
+        )
+
+    def generate(self, input: torch.Tensor, max_len: int = 1000):
+        '''
+        Input Shape: (B, T)
+        Adds additional tokens to the input such that
+        output has shape (B, T + max_len)
+        '''
+        output = input # (B, T)
+        for _ in range(max_len):
+            logits = self(output[:, -block_size:]) # (B, T, C)
+            probs = F.softmax(logits[:, -1, :], dim=-1) # (B, C)
+            next_token = torch.multinomial(probs, 1) # (B, 1)
+            output = torch.cat([output, next_token], dim=1) # (B, T + 1)
+
+        return output
 
 
 if __name__ == "__main__":
     trainer = Trainer(
         max_iters=10000,
         batch_size=12,
-        block_size=32,
+        block_size=block_size,
         eval_interval=2000,
         eval_iters=200,
     )
     # model = BiGramModel(vocab_size=vocab_size)
-    model = BiGramWithSingleHeadAttention(trainer.vocab_size, n_embd=32, head_dim=32, block_size=trainer.block_size)
+    model = BiGramWithBlocks(trainer.vocab_size, n_embd=32, head_dim=32)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     
     # train the model
